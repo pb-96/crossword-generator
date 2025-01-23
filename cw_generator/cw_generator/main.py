ca79621875264c5dd676e9677466d077f0cf0ada
pb-96 <philip.byrne@faculty.ai>
2025-01-23 09:49:49 +0000

    removing venv

---
+++ cw_generator/cw_generator/main.py
@@ -0,0 +1,8 @@
+from cw_generator.cw_generator.cd_generator import CrossWordGen
+from cw_generator.cw_generator.cd_validator import WordSearch
+from typing import List, Dict, Tuple
+
+def main(to_place: List[str]) -> Dict[str, Tuple]:
+    ...
+
+    
