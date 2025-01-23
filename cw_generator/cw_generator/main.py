3f79a6ec5141033e0ded02be3fd5516915de323a
pb-96 <philip.byrne@faculty.ai>
2025-01-23 09:56:34 +0000

    adding main script

--- cw_generator/cw_generator/main.py
+++ cw_generator/cw_generator/main.py
@@ -1,8 +1,18 @@
 from cw_generator.cw_generator.cd_generator import CrossWordGen
-from cw_generator.cw_generator.cd_validator import WordSearch
-from typing import List, Dict, Tuple
+from cw_generator.cw_generator.cd_validator import WordSearch, Point
+from typing import List, Dict, Tuple, cast
+
 
 def main(to_place: List[str]) -> Dict[str, Tuple]:
-    ...
+    cross_word_cls = CrossWordGen(to_place)
+    searcher = WordSearch(puzzle=cross_word_cls.cw_matrix)
 
-    
+    points = {}
+    for word in to_place:
+        point = searcher.search(word)
+        if point is None:
+            continue
+        point = cast(Point, point)
+        points[word] = (point.x, point.y)
+
+    return points
